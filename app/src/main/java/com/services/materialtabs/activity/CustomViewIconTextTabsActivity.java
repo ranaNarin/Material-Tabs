80461c88dc11e5327f0d797ac3320ddd16855560
rana.narin <rana.narin@gmail.com>
2017-12-13 10:58:15 +0530

    done

--- app/src/main/java/com/services/materialtabs/activity/CustomViewIconTextTabsActivity.java
+++ app/src/main/java/com/services/materialtabs/activity/CustomViewIconTextTabsActivity.java
@@ -8,7 +8,9 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.TextView;
 
 import com.services.materialtabs.R;
@@ -56,16 +58,19 @@ public class CustomViewIconTextTabsActivity extends AppCompatActivity {
         tabOne.setText("ONE");
         tabOne.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_tab_favourite, 0, 0);
         tabLayout.getTabAt(0).setCustomView(tabOne);
+        tabOne.setGravity(Gravity.CENTER);
 
         TextView tabTwo = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
         tabTwo.setText("TWO");
         tabTwo.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_tab_call, 0, 0);
         tabLayout.getTabAt(1).setCustomView(tabTwo);
+        tabTwo.setGravity(Gravity.CENTER);
 
         TextView tabThree = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
         tabThree.setText("THREE");
         tabThree.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_tab_contacts, 0, 0);
         tabLayout.getTabAt(2).setCustomView(tabThree);
+        tabThree.setGravity(Gravity.CENTER);
     }
 
     /**
