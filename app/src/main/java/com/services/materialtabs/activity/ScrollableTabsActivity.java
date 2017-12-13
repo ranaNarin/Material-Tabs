dccfb60a7a882f4797887eda080648cba7d31f28
rana.narin <rana.narin@gmail.com>
2017-12-13 10:52:18 +0530

    Material Tabs
    Simple Tabs
    Scrollable Tabs
    Icon & text Tabs
    Only icon Tabs
    custom view icon & text

---
+++ app/src/main/java/com/services/materialtabs/activity/ScrollableTabsActivity.java
@@ -0,0 +1,97 @@
+package com.services.materialtabs.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.services.materialtabs.R;
+import com.services.materialtabs.fragments.EightFragment;
+import com.services.materialtabs.fragments.FiveFragment;
+import com.services.materialtabs.fragments.FourFragment;
+import com.services.materialtabs.fragments.NineFragment;
+import com.services.materialtabs.fragments.OneFragment;
+import com.services.materialtabs.fragments.SevenFragment;
+import com.services.materialtabs.fragments.SixFragment;
+import com.services.materialtabs.fragments.TenFragment;
+import com.services.materialtabs.fragments.ThreeFragment;
+import com.services.materialtabs.fragments.TwoFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by narinder on 13/12/17.
+ */
+
+
+public class ScrollableTabsActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrollable_tabs);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new OneFragment(), "ONE");
+        adapter.addFrag(new TwoFragment(), "TWO");
+        adapter.addFrag(new ThreeFragment(), "THREE");
+        adapter.addFrag(new FourFragment(), "FOUR");
+        adapter.addFrag(new FiveFragment(), "FIVE");
+        adapter.addFrag(new SixFragment(), "SIX");
+        adapter.addFrag(new SevenFragment(), "SEVEN");
+        adapter.addFrag(new EightFragment(), "EIGHT");
+        adapter.addFrag(new NineFragment(), "NINE");
+        adapter.addFrag(new TenFragment(), "TEN");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFrag(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+}
